cf03926611fdc06c7b083442b52d197b1ecdd1fd
adrianmontes23 <ramirezjulian4321@gmail.com>
2025-02-20 22:07:27 -0600

    Removed print statements used for debugging in main.py

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import sys
 argument = sys.argv[1]
 
 if argument.lower() == "wifi":
-    print("Wifi")
     os.system("netsh interface set interface Ethernet disable")
 elif argument.lower() == "eth":
-    print("Ethernet")
     os.system("netsh interface set interface Ethernet enable")
 else:
     print("Invalid argument")
